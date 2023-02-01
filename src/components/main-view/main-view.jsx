c9d18dcfc9cbb75c12e754a37ed8b66822c75a6a
unknown <reman1999@outlook.com>
2023-02-01 22:00:49 +0100

    Revert "Revert "Fixed some minor issues""
    This reverts commit 383d1430a45b49d4bb07efed1057fe48579c8227.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+<<<<<<< Updated upstream
 import { useState, useEffect } from 'react';
 
 export const MainView = () => {
@@ -9,8 +10,22 @@ export const MainView = () => {
   const [selectedMovie, setSelectedMovie] = useState(null);
 
   // Fetch movie API (HAS NOT BEEN INSERTED YET)
+=======
+import { LoginView } from '../login-view/login-view';
+import { SignupView } from '../signup-view/signup-view';
+import { NavigationBar } from '../navigation-bar/navigation-bar';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+export const MainView = () => {
+  const [movies, setMovies] = useState([]);
+  const [user, setUser] = useState(null);
+
+  // FETCH API
+>>>>>>> Stashed changes
   useEffect(() => {
-    fetch('#')
+    fetch('https://reman-backend.onrender.com/movies')
       .then((response) => response.json())
       .then((data) => {
         const moviesFromApi = data.docs.map((doc) => {
@@ -21,10 +36,13 @@ export const MainView = () => {
             author: doc.author_name?.[0],
           };
         });
+<<<<<<< Updated upstream
 
         setMovies(moviesFromApi);
       });
   }, []);
+=======
+>>>>>>> Stashed changes
 
   if (selectedMovie) {
     return (
@@ -40,6 +58,7 @@ export const MainView = () => {
   }
 
   return (
+<<<<<<< Updated upstream
     <div>
       {movies.map((movie) => (
         <MovieCard
@@ -51,5 +70,84 @@ export const MainView = () => {
         />
       ))}
     </div>
+=======
+    <BrowserRouter>
+      <NavigationBar
+        user={user}
+        onLoggedOut={() => {
+          setUser(null);
+        }}
+      />
+      <Row className="justify-content-md-center">
+        <Routes>
+          <Route
+            path="/signup"
+            element={
+              <>
+                {user ? (
+                  <Navigate to="/" />
+                ) : (
+                  <Col md={5}>
+                    <SignupView />
+                  </Col>
+                )}
+              </>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <>
+                {user ? (
+                  <Navigate to="/" />
+                ) : (
+                  <Col md={5}>
+                    <LoginView onLoggedIn={(user) => setUser(user)} />
+                  </Col>
+                )}
+              </>
+            }
+          />
+          <Route
+            path="/movies/:movieId"
+            element={
+              <>
+                {!user ? (
+                  <Navigate to="/login" replace />
+                ) : movies.length === 0 ? (
+                  <Col>The list is empty!</Col>
+                ) : (
+                  <Col md={8}>
+                    <MovieView movies={movies} />
+                  </Col>
+                )}
+              </>
+            }
+          />
+
+          <Route
+            path="/"
+            element={
+              <>
+                {!user ? (
+                  <Navigate to="/login" replace />
+                ) : movies.length === 0 ? (
+                  <Col>The list is empty!</Col>
+                ) : (
+                  <>
+                    {movies.map((movie) => (
+                      <Col className="mb-4" key={movie.id} md={3}>
+                        <MovieCard movie={movie} />
+                      </Col>
+                    ))}
+                  </>
+                )}
+              </>
+            }
+          />
+        </Routes>
+      </Row>
+    </BrowserRouter>
+>>>>>>> Stashed changes
   );
 };
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,4 +1,6 @@
+import React from 'react';
 import PropTypes from 'prop-types';
+<<<<<<< Updated upstream
 
 // The BookCard function component
 export const MovieCard = ({ movie, onMovieClick }) => {
@@ -10,13 +12,30 @@ export const MovieCard = ({ movie, onMovieClick }) => {
     >
       {movie.title}
     </div>
+=======
+import { Button, Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+export const MovieCard = ({ movie }) => {
+  return (
+    <Card>
+      <Card.Img variant="top" src={movie.image} />
+      <Card.Body>
+        <Card.Title>{movie.title}</Card.Title>
+        <Card.Text>{movie.author}</Card.Text>
+        <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
+          <Button variant="link">Open</Button>
+        </Link>
+      </Card.Body>
+    </Card>
+>>>>>>> Stashed changes
   );
 };
 
-// Here is where we define all the props constraints for the MovieCard
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    title: PropTypes.string,
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    author: PropTypes.string,
   }).isRequired,
-  onMovieClick: PropTypes.func.isRequired,
 };
