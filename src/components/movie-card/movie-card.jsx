383d1430a45b49d4bb07efed1057fe48579c8227
unknown <reman1999@outlook.com>
2023-02-01 21:43:16 +0100

    Revert "Fixed some minor issues"
    This reverts commit 014fc6fd2a08062b385d882670e405a9281cb297.

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,6 +1,4 @@
-import React from 'react';
 import PropTypes from 'prop-types';
-<<<<<<< Updated upstream
 
 // The BookCard function component
 export const MovieCard = ({ movie, onMovieClick }) => {
@@ -12,30 +10,13 @@ export const MovieCard = ({ movie, onMovieClick }) => {
     >
       {movie.title}
     </div>
-=======
-import { Button, Card } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-
-export const MovieCard = ({ movie }) => {
-  return (
-    <Card>
-      <Card.Img variant="top" src={movie.image} />
-      <Card.Body>
-        <Card.Title>{movie.title}</Card.Title>
-        <Card.Text>{movie.author}</Card.Text>
-        <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
-          <Button variant="link">Open</Button>
-        </Link>
-      </Card.Body>
-    </Card>
->>>>>>> Stashed changes
   );
 };
 
+// Here is where we define all the props constraints for the MovieCard
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
-    author: PropTypes.string,
+    title: PropTypes.string,
   }).isRequired,
+  onMovieClick: PropTypes.func.isRequired,
 };
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,36 +1,18 @@
-<<<<<<< Updated upstream
 export const MovieView = ({ movie, onBackClick }) => {
-=======
-import { useParams } from 'react-router';
-import { Link } from 'react-router-dom';
-import './movie-view.scss';
-
-export const MovieView = ({ movies }) => {
-  const { movieId } = useParams();
-
-  const movie = movies.find((b) => b.id === movieId);
-
->>>>>>> Stashed changes
   return (
     <div>
       <div>
-        <img className="w-100" src={movie.image} />
+        <img src={movie.image} />
       </div>
       <div>
         <span>Title: </span>
         <span>{movie.title}</span>
       </div>
       <div>
-        <span>Author: </span>
-        <span>{movie.author}</span>
+        <span>Director: </span>
+        <span>{movie.director}</span>
       </div>
-<<<<<<< Updated upstream
       <button onClick={onBackClick}>Back</button>
-=======
-      <Link to={`/`}>
-        <button className="back-button">Back</button>
-      </Link>
->>>>>>> Stashed changes
     </div>
   );
 };
